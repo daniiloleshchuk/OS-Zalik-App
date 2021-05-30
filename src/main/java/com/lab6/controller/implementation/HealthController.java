87a2d75cb4e8c6241627176e6d703325eb60c366
Daniil Oleshchuk <danieloleshuk@gmail.com>
2021-05-30 22:52:45 +0300

    added health controller

---
+++ src/main/java/com/lab6/controller/implementation/HealthController.java
@@ -0,0 +1,15 @@
+package com.lab6.controller.implementation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class HealthController {
+
+    @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+    public ResponseEntity returnHealthCheckResponse() {
+        return new ResponseEntity<>("OKKKKKKKKK", HttpStatus.OK);
+    }
+}
